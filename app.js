d859e5d4d21948d052df7ffd1a3ee2566df798b8
Lucas Lafèré <laferelucas@gmail.com>
2022-06-16 16:08:07 -0300

    added all functions

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+const users = [{
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+}];
+
+const tweets = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}];
+
+
+app.post("/sign-up", (req, res) => {
+    users.push({
+        username: req.body.username,
+        avatar: req.body.avatar
+    });
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    tweets.push({
+        username: req.body.username,
+        tweet: req.body.tweet
+    });
+    res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    const last10 = tweets.slice(-10);
+    res.send(last10);
+})
 
 
 app.listen(5000);
